261081634b86e5d200e4c5a12973c03a9535d06f
demu <demura@neptune.kanazawa-it.ac.jp>
2024-03-14 22:50:53 +0900

    Humble

--- turtlesim_launch/mysim.launch.py
+++ turtlesim_launch/mysim.launch.py
@@ -1,17 +1,18 @@
-import launch
-import launch_ros.actions
+from launch import actions
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
 def generate_launch_description():
-    return launch.LaunchDescription([
-        launch_ros.actions.Node(
-            namespace= "turtlesim1", package='turtlesim', executable='turtlesim_node', output='screen', respawn='true',
+    return LaunchDescription([
+        Node(
+            namespace= "turtlesim1", package='turtlesim', executable='turtlesim_node', output='screen',  
             remappings=[('/cmd_vel', '/kame/cmd_vel')]),
-        launch_ros.actions.Node(
-            namespace= "turtlesim2", package='turtlesim', executable='turtlesim_node', output='screen', respawn='true',
+        Node(
+            namespace= "turtlesim2", package='turtlesim', executable='turtlesim_node', output='screen', 
             parameters=[{"background_b": 200}, {"background_g": 200},{"background_r": 200}]),
-        launch_ros.actions.Node(
+        Node(
             prefix='xterm -e', namespace= "turtlesim1", package='turtlesim', executable='turtle_teleop_key', output='screen',
-            on_exit=launch.actions.Shutdown()),
-        launch_ros.actions.Node(
+            on_exit=actions.Shutdown()),
+        Node(
             prefix='xterm -e', namespace= "turtlesim2", package='turtlesim', executable='turtle_teleop_key', output='screen')
     ])
