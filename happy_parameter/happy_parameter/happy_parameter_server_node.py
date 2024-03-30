34fc012442000671ec96e8a8c4c940467b90e161
demu <demura@neptune.kanazawa-it.ac.jp>
2024-03-30 12:40:51 +0900

    新規作成

---
+++ happy_parameter/happy_parameter/happy_parameter_server_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from happy_parameter.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ happy_parameter/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'happy_parameter'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='demura@neptune.kanazawa-it.ac.jp',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'happy_parameter_server_node = happy_parameter.happy_parameter_server_node:main'
+        ],
+    },
+)
