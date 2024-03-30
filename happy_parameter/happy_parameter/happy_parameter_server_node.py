27cb01fce6bbd068279df66c65d86c08c17c5ae4
demu <demura@neptune.kanazawa-it.ac.jp>
2024-03-30 12:44:54 +0900

    新規作成

--- happy_parameter/happy_parameter/happy_parameter_server_node.py
+++ happy_parameter/happy_parameter/happy_parameter_server_node.py
@@ -1,6 +1,23 @@
-def main():
-    print('Hi from happy_parameter.')
+import rclpy
+from rclpy.node import Node
 
+# ParameterServerクラスを定義します。Nodeクラスを継承しています。
+class ParameterServer(Node):
+    def __init__(self):
+        # ノード名を'parameter_server'に設定します。
+        super().__init__('parameter_server')
+        # 'example_parameter'という名前のパラメータを宣言し、初期値を'Hello World'に設定します。
+        self.declare_parameter('example_parameter', 'Hello World')
+
+# メイン関数
+def main(args=None):
+    rclpy.init(args=args)  # ROS 2のPythonクライアントライブラリを初期化
+    node = ParameterServer()  # ParameterServerクラスのインスタンスを作成
+    try:
+        rclpy.spin(node)  # ノードをspinし、コールバック関数を実行可能にします。
+    except KeyboardInterrupt:
+        pass  # キーボード割り込み（Ctrl+C）が発生した場合は終了します。
+    rclpy.shutdown()  # ROS 2のシャットダウン処理
 
 if __name__ == '__main__':
     main()
