9c05cc5cbba2b8e6bb714570ce3cfb095eaa0105
demuko <demura@neptune.kanazawa-it.ac.jp>
2024-09-30 00:06:28 +0900

    first commit

---
+++ bringme_action/bringme_action/bringme_action_client_node.py
@@ -0,0 +1,45 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.action import ActionClient
+from airobot_interfaces.action import StringCommand
+
+class BringmeActionClient(Node):
+    def __init__(self):
+        super().__init__('bringme_action_client_node')
+        self._action_client = ActionClient(self, StringCommand, 'command')
+
+    def send_goal(self, order):
+        goal_msg = StringCommand.Goal()
+        goal_msg.command = order
+        self._action_client.wait_for_server()
+        self._send_goal_future = self._action_client.send_goal_async(
+            goal_msg, feedback_callback=self.feedback_callback
+        )
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('ゴールは拒否されました')
+            return
+
+        self.get_logger().info('ゴールが承認されました')
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    def feedback_callback(self, feedback_msg):
+        self.get_logger().info(f'フィードバック: 進捗 {feedback_msg.progress}%')
+
+    def get_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info(f'最終結果: {result.answer}')
+        rclpy.shutdown()
+
+def main(args=None):
+    rclpy.init(args=args)
+    bringme_action_client = BringmeActionClient()
+
+    order = input('何を取ってきますか：')
+    bringme_action_client.send_goal(order)
+
+    rclpy.spin(bringme_action_client)
---
+++ bringme_action/bringme_action/bringme_action_server_node.py
@@ -0,0 +1,69 @@
+import time
+import threading
+import rclpy
+from rclpy.node import Node
+from rclpy.action import ActionServer, GoalResponse, CancelResponse
+from rclpy.callback_groups import ReentrantCallbackGroup
+from airobot_interfaces.action import StringCommand  # アクションファイルのインポート
+
+class BringmeActionServer(Node):
+    def __init__(self):
+        super().__init__('bringme_action_server')
+        self._action_server = ActionServer(
+            self,
+            StringCommand,
+            'command',
+            execute_callback=self.execute_callback,
+            goal_callback=self.goal_callback,
+            cancel_callback=self.cancel_callback,
+            handle_accepted_callback=self.handle_accepted_callback,
+            callback_group=ReentrantCallbackGroup()
+        )
+        self.food = ['apple', 'banana', 'candy']
+
+    def goal_callback(self, goal_request):
+        self.get_logger().info(f"ゴールリクエストが来ました．コマンド: {goal_request.command}")
+        return GoalResponse.ACCEPT
+
+    def cancel_callback(self, goal_handle):
+        self.get_logger().info(f"キャンセルリクエストを受け取りました．")
+        return CancelResponse.ACCEPT
+
+    def handle_accepted_callback(self, goal_handle):
+        self.get_logger().info("ゴールが承認されました。非同期で処理を開始します。")
+        threading.Thread(target=self.execute_callback, args=(goal_handle,)).start()
+
+    def execute_callback(self, goal_handle):
+        feedback_msg = StringCommand.Feedback()
+        feedback_msg.progress = 0
+
+        for i in range(5):
+            if goal_handle.is_cancel_requested:
+                goal_handle.canceled()
+                self.get_logger().info('ゴールがキャンセルされました。')
+                return StringCommand.Result()
+
+            time.sleep(1)
+            feedback_msg.progress += 20
+            self.get_logger().info(f"フィードバック送信中: {feedback_msg.progress}%")
+            goal_handle.publish_feedback(feedback_msg)
+
+        result = StringCommand.Result()
+        for item in self.food:
+            if item in goal_handle.request.command:
+                result.answer = 'はい，これです．'
+                goal_handle.succeed()
+                self.get_logger().info(f"ゴールの結果: {result.answer}")
+                return result
+        
+        result.answer = '見つけることができませんでした．'
+        goal_handle.succeed()
+        self.get_logger().info(f"ゴールの結果: {result.answer}")
+        return result
+
+def main(args=None):
+    rclpy.init(args=args)
+    bringme_action_server = BringmeActionServer()
+    rclpy.spin(bringme_action_server)
+    bringme_action_server.destroy_node()
+    rclpy.shutdown()
