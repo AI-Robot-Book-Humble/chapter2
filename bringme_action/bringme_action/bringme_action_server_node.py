70a2cb88a2d4f955eede21b5ba7ceee133a490e2
demu <demura@neptune.kanazawa-it.ac.jp>
2024-10-14 14:40:51 +0900

    升谷先生の修正を反映

--- bringme_action/bringme_action/bringme_action_server_node.py
+++ bringme_action/bringme_action/bringme_action_server_node.py
@@ -1,8 +1,7 @@
 import time, random
-import threading
 import rclpy
 from rclpy.node import Node
-from rclpy.action import ActionServer, GoalResponse
+from rclpy.action import ActionServer
 from airobot_interfaces.action import StringCommand  # カスタムアクション定義のインポート
 
 
@@ -11,37 +10,39 @@ class BringmeActionServer(Node):
         super().__init__('bringme_action_server')
         self._action_server = ActionServer(
             self, StringCommand, 'command', 
-            execute_callback=self.execute_callback            
+            execute_callback=self.execute_callback,
         )
         self.food = ['apple', 'banana', 'candy']
 
     def execute_callback(self, goal_handle):
         feedback = StringCommand.Feedback()
-        count = random.randint(10, 20)
+        count = random.randint(5, 10)
 
         while count > 0:
-            self.get_logger().info(f"フィードバック送信中: 残り{count}[s]")     
+            self.get_logger().info(f'フィードバック送信中: 残り{count}[s]')     
             feedback.process = f'{count}'
             goal_handle.publish_feedback(feedback)  
             count -= 1  
             time.sleep(1)
 
+        item = goal_handle.request.command
         result = StringCommand.Result()
-        for item in self.food:
-            if item in goal_handle.request.command:
-                result.answer =f'はい，{item}です．'
-                goal_handle.succeed()
-                self.get_logger().info(f"ゴールの結果: {result.answer}")
-                return result
-        
-        result.answer = f'{goal_handle.request.command}を見つけることができませんでした．'
+        if item in self.food:
+            result.answer =f'はい，{item}です．'
+        else:
+            result.answer = f'{item}を見つけることができませんでした．'
         goal_handle.succeed()
-        self.get_logger().info(f"ゴールの結果: {result.answer}")
+        self.get_logger().info(f'ゴールの結果: {result.answer}')
         return result
 
-def main(args=None):
-    rclpy.init(args=args)
+
+def main():
+    rclpy.init()
     bringme_action_server = BringmeActionServer()
-    rclpy.spin(bringme_action_server)
-    bringme_action_server.destroy_node()
-    rclpy.shutdown()
+    print('サーバ開始')
+    try:
+        rclpy.spin(bringme_action_server)
+    except KeyboardInterrupt:
+        pass
+    rclpy.try_shutdown()
+    print('サーバ終了')
